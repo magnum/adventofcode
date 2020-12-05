050ead695617b9fa16dc8a8fa1a4d3c0af59c594
Antonio Molinari <molinari@incode.it>
2020-12-05 15:14:00 +0100

    completed day 5

---
+++ day5_2.js
@@ -0,0 +1,40 @@
+let passes = [];
+fetch('https://adventofcode.com/2020/day/5/input')
+.then(function(response) {
+    response.text().then(input => {
+        passes = input.split("\n")
+        .map( r => r.trim())
+        .filter( r => r!="")
+        .map(p => {
+            // rows
+            let rl = 0;
+            let rr = 128;
+            let r = null;
+            for(let i=0; i<7; i++) {
+                p[i] == "F" ? rr -= (rr-rl)/2 : rl += (rr-rl)/2;
+                if(rr-rl == 1) r = p[i] == "F" ? rl : rr-1;
+            }
+            // columns
+            let cl = 0;
+            let cr = 8;
+            c = null;
+            for(let i=7; i<10; i++) {
+                p[i] == "L" ? cr -= (cr-cl)/2 : cl += (cr-cl)/2;
+                if(cr-cl == 1) c = p[i] == "L" ? cl : cr-1;
+            }
+            return  {
+                pass: p,
+                row: r,
+                column: c,
+                id: r*8+c,
+            }
+        })
+        .sort((a, b) => b.id - a.id);
+        console.log("pass w/ highest id: "+passes[0].id);
+
+        for(let i = passes[passes.length-1].id; i< passes[0].id; i++) {
+            p = passes.find( p => p.id == i)
+            if(!p) console.log("missing pass id: "+i);
+        }
+    });
+});
