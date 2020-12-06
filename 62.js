c16bf41ba0abfb644e363991d5a6b7ebe8252f24
Antonio Molinari <molinari@incode.it>
2020-12-06 14:54:14 +0100

    trying again..

--- 62.js
+++ 62.js
@@ -1,20 +1,20 @@
 fetch('https://adventofcode.com/2020/day/6/input')
-.then(function(response) {
+.then(response => {
     response.text().then(input => {
         answers = input.split("\n\n")
         .map( r => r.split("\n"))
         .map( r => r.filter( r => r != ""))
 
-        let parseAnswer = (a) => [... new Set(a.join("").split(""))]
+        let uniqueAnswers = (a) => [... new Set(a.join("").split(""))]
 
         // part 1
         console.log(
-            answers.map(a => parseAnswer(a).length).reduce( (s, c) => s+c)
+            answers.map(a => uniqueAnswers(a).length).reduce( (s, c) => s+c)
         )
 
         // part 2
         console.log(
-            answers.map(a => parseAnswer(a)
+            answers.map(a => uniqueAnswers(a)
                 .map( v => a.join("").split("").filter(i => i==v).length)
                 .filter( v => v >= a.length)
             ).map(a => a.length).reduce( (s,i) => s+i)
