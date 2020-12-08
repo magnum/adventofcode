ede108673df6b34daf427010b45bcf85e5ce6785
Antonio Molinari <molinari@incode.it>
2020-12-08 14:57:05 +0100

    polishing

--- 2020/82.rb
+++ 2020/82.rb
@@ -1,48 +1,31 @@
 require 'open-uri'
 require 'pry'
 
-
-def execute(instructions)
+def execute(instructions, only_complete=false)
   steps = []
-  acc = 0
-  i = 0
-  while steps.length < 2000
+  acc = i = 0
+  while steps.length < instructions.count*10
     c,p = instructions[i]
-    case c
-      when "acc"
-        acc += (p.to_i)
-        i += 1
-      when "jmp"
-        n = (p.to_i)
-        #n += 1 if p.index("+")
-        i += n
-      else
-        i += 1
-    end 
-    break if steps.index(i)
-    break if i == instructions.length
+    i+= c == "jmp" ? p.to_i : 1
+    acc += (p.to_i) if c == "acc"
+    break if steps.index(i) || (i == instructions.length)
     steps << i
   end
   completed = (i == instructions.length)
-  puts "acc #{acc}, steps: #{steps.length}, i: #{i}, completed? #{completed}" if completed
+  puts "acc #{acc}, steps: #{steps.length}, i: #{i}, completed? #{completed}" if !only_complete || (only_complete && completed)
   i
 end
 
+#part 1
+statements = open('8.txt') {|f| f.read }.split("\n")
+.map{|s| s.split(" ")}
+execute(statements)
 
-# statements = open('8.test.txt') {|f| f.read }.split("\n").map{|s| s.split(" ")}
-# execute(statements)
-# exit #test
-
-
-statements = open('8.txt') {|f| f.read }.split("\n").map{|s| s.split(" ")}
+#part 2
 statements.each_with_index do |s, i|
-  fixes = Marshal.load(Marshal.dump(statements))
-  if ["jmp", "nop"].index(fixes[i][0])
-    fixes[i][0] = fixes[i][0] == "jmp" ? "nop" : "jmp"
-    #puts "line #{i} from #{statements[i]} to #{fixes[i]} "
-    execute(fixes)
-  end
-  execute(fixes)
+  fixes = statements.map{|s| s.clone}
+  fixes[i][0] = fixes[i][0] == "jmp" ? "nop" : "jmp" if ["jmp", "nop"].index(fixes[i][0])
+  execute(fixes, true)
 end
 
 
