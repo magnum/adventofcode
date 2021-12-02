16d23b24fef53b4984cf770eb9eecd9d73a03acf
Antonio Molinari <molinari@incode.it>
2021-12-02 08:46:15 +0100

    day2

--- 2021/2.rb
+++ 2021/2.rb
@@ -1,8 +1,41 @@
-lines = open('2.test.txt') {|f| f.read }
+lines = open('2.txt') {|f| f.read }
 .split("\n")
-.map(&:to_i)
 
 #part 1
+h = 0
+d = 0
 lines.each_with_index do |line, i|
-  puts line
+  command, x = line.split(" ")
+  x = x.to_i
+  case command
+    when "forward"
+      h += x
+    when "up"
+      d -= x
+    when "down"
+      d += x
+  end
+  puts "commman: #{command}, x: #{x}"
 end
+result = d*h
+puts "h: #{h}, d: #{d}, result: #{result}"
+
+#part 2
+h = 0
+d = 0
+aim = 0
+lines.each_with_index do |line, i|
+  command, x = line.split(" ")
+  x = x.to_i
+  case command
+    when "forward"
+      h += x ; d += aim*x
+    when "up"
+      aim -= x
+    when "down"
+      aim += x
+  end
+  puts "commman: #{command}, x: #{x}"
+end
+result = d*h
+puts "h: #{h}, d: #{d}, result: #{result}"
