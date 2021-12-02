70b6414636467d3a4858743ef2c268756eebde00
Antonio Molinari <molinari@incode.it>
2021-12-02 02:02:32 +0100

    testing 1a in js

--- 2021/1.rb
+++ 2021/1.rb
@@ -4,21 +4,19 @@ numbers = open('1.txt') {|f| f.read }
 
 #part 1
 prev = nil
-increases = 0
+count = 0
 numbers.each_with_index do |n, i|
-  increases +=1 if prev && n > prev
+  count +=1 if prev && n > prev
+  #puts "#{n} > #{prev} ? #{n > prev rescue false}, count: #{count}"
   prev = n
 end
-puts "count increases : #{increases}"
+puts "increases: #{count}"
 
 #part 2
-windows_increases = 0
+increases = 0
 numbers.each_with_index do |n, i|
   window_prev = numbers[(i-3)..(i-1)]
   windows_current = numbers[(i-2)..i]
-  puts "#{(i-2)} - #{i}"
-  puts windows_current
-  puts "---"
-  windows_increases +=1 if window_prev.length == 3 && windows_current.inject(0){|sum,i| sum+=i} > window_prev.inject(0){|sum,i| sum+=i}
+  increases +=1 if window_prev.length == 3 && windows_current.inject(0){|sum,i| sum+=i} > window_prev.inject(0){|sum,i| sum+=i}
 end
-puts "count window_increases : #{windows_increases}"
+puts "grouped increases: #{increases}"
