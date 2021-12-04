b05c1ae3ff0c0f0aaaa44513fac1eb5f4075ce31
Antonio Molinari <molinari@incode.it>
2021-12-04 18:26:23 +0100

    wip for day4, part2

--- 2021/4.rb
+++ 2021/4.rb
@@ -1,79 +1,44 @@
 require 'pry'
 
-lines = open('4.txt') {|f| f.read }
+lines = open('4.test.txt') {|f| f.read }
 .split("\n")
 
-class BoardLine
-  attr_accessor :index
-  attr_accessor :numbers
-  attr_accessor :numbers_marked
-  def initialize(index, numbers)
-    @index = index
-    @numbers = numbers
-    @numbers_marked = []
-  end
-  def mark(number)
-    @numbers_marked << number if @numbers.index(number)
-  end
-  def numbers_unmarked
-    @numbers - @numbers_marked
-  end
-  def marked?
-    @numbers.length == @numbers_marked.length 
-  end
-end
-
-class Board
-  attr_accessor :lines
-  def initialize()
-    @lines = []
-  end
-  def set_line(i, numbers)
-    @lines[i] = BoardLine.new(i, numbers)
-  end
-  def numbers
-    lines.map(&:numbers).flatten
-  end
-  def numbers_marked
-    lines.map(&:numbers_marked).flatten
-  end
-  def numbers_unmarked
-    lines.map(&:numbers_unmarked).flatten
-  end
-  def marked_lines
-    @lines.find_all(&:marked?)
-  end
-  def mark(number)
-    @lines.each{|l| l.mark number}
-  end
-  def marked_lines?
-    marked_lines.length > 0
-  end
-  def marked?
-    @lines.length == marked_lines.length
-  end
-end
-
+board = []
 boards = []
 drawings = []
-board = nil
 lines.each_with_index do |line, i|
   if i == 0
     drawings = line.split(",").map(&:to_i)
   else 
     lnumber =  (i-1) % 6
     #puts "#{i} #{lnumber}"
-    board = Board.new if lnumber == 0
-    board.set_line (lnumber-1), line.split(" ").map(&:strip).map(&:to_i) if lnumber > 0
+    board = [] if lnumber == 0
+    board[lnumber-1] =  line.split(" ").map(&:strip).map(&:to_i) if lnumber > 0
     boards << board if lnumber == 5
   end
 end
 
+def board_wins?(board)
+  (board.find_all{|line| line.count("x") == line.length}.length > 0) #|| 
+  (board.transpose.find_all{|line| line.count("x") == line.length}.length > 0)
+end
+
+def boards_print(b)
+  b.map{|l| l.map{|n| n=="x" ? "\e[31m#{n}\e[0m" : n}.join(",")}.join("\n")
+end
+
 drawings.each do |d|
-  boards.each_with_index do |board, boards_index|
-    if board.mark(d) && board.marked_lines.length > 0
-      puts "with number #{d}, board #{boards_index} has #{board.marked_lines.length} marked line(s), result: #{board.numbers_unmarked.inject(0){|sum, n| sum+n}*d}"
-      return 
+  puts "\nEXTRACTING #{d}"
+  boards = boards.map{|board| board.map{|line| line.map{|n| n==d ? "x" : n}}} #.delete_if{|board| board.find_all{|line| line.length == 0}.length > 0}
+  boards.each_with_index do |board, i|
+    if board_wins?(board)
+      puts "with #{d}, board #{i}, wins w/result #{board.flatten.delete_if{|n| n=="x"}.inject(0){|sum, n| sum+n}*d}" 
+      break
     end
   end
+  break if boards.length == 0
+  puts boards.map.with_index{|b,i| "board #{i}\n#{boards_print(b)}\ntransposed\n#{boards_print(b.transpose)}\n"}.join("\n")
+  #binding.pry
 end
+
+
