b1e34cd73ffbbd89c23b884c5249d076fc179dc0
Antonio Molinari <molinari@incode.it>
2021-12-05 00:36:38 +0100

    day4, part b finally

--- 2021/4.rb
+++ 2021/4.rb
@@ -1,17 +1,16 @@
 require 'pry'
 
-lines = open('4.test.txt') {|f| f.read }
+lines = open('4.txt') {|f| f.read }
 .split("\n")
 
+CHECK = "X"
 board = []
 boards = []
 drawings = []
 lines.each_with_index do |line, i|
-  if i == 0
-    drawings = line.split(",").map(&:to_i)
-  else 
+  drawings = line.split(",").map(&:to_i) if i == 0
+  if i > 0
     lnumber =  (i-1) % 6
-    #puts "#{i} #{lnumber}"
     board = [] if lnumber == 0
     board[lnumber-1] =  line.split(" ").map(&:strip).map(&:to_i) if lnumber > 0
     boards << board if lnumber == 5
@@ -19,26 +18,24 @@ lines.each_with_index do |line, i|
 end
 
 def board_wins?(board)
-  (board.find_all{|line| line.count("x") == line.length}.length > 0) #|| 
-  (board.transpose.find_all{|line| line.count("x") == line.length}.length > 0)
+  (board.find_all{|line| line.count(CHECK) == line.length}.length > 0) || 
+  (board.transpose.find_all{|line| line.count(CHECK) == line.length}.length > 0)
 end
 
 def boards_print(b)
-  b.map{|l| l.map{|n| n=="x" ? "\e[31m#{n}\e[0m" : n}.join(",")}.join("\n")
+  puts b.map{|l| l.map{|n| n==CHECK ? "\e[31m#{n} \e[0m" : n.to_s.rjust(2," ")}.join(",")}.join("\n")
 end
 
+
 drawings.each do |d|
-  puts "\nEXTRACTING #{d}"
-  boards = boards.map{|board| board.map{|line| line.map{|n| n==d ? "x" : n}}} #.delete_if{|board| board.find_all{|line| line.length == 0}.length > 0}
+  boards = boards.map{|board| board.map{|line| line.map{|n| n==d ? CHECK : n}}} 
   boards.each_with_index do |board, i|
-    if board_wins?(board)
-      puts "with #{d}, board #{i}, wins w/result #{board.flatten.delete_if{|n| n=="x"}.inject(0){|sum, n| sum+n}*d}" 
-      break
+    if board_wins?(board) 
+      boards.delete(board)
+      puts "with #{d}, board #{i} wins w/ result #{board.flatten.delete_if{|n| n==CHECK}.inject(0){|sum, n| sum+n}*d}" 
+      boards_print board
     end
   end
-  break if boards.length == 0
-  puts boards.map.with_index{|b,i| "board #{i}\n#{boards_print(b)}\ntransposed\n#{boards_print(b.transpose)}\n"}.join("\n")
-  #binding.pry
 end
 
 
