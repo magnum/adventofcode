3a93989e6b16ad39a84a06d268ca344a4452362a
antonio molinari <antoniomolinari@me.com>
2022-12-02 19:57:04 +0100

    day2

--- 2022/day2.rb
+++ 2022/day2.rb
@@ -1,8 +1,31 @@
-require 'httparty'
-require 'pry'
-
-#lines = HTTParty.get 'https://adventofcode.com/2022/day/1/input
-lines = File.open("input1a.txt").read.split("\n\n").map{|l| l.split("\n")}
-sums = lines.map{|a| a.inject(0){|sum,b| sum+=b.to_i}}.sort
-puts "part1: #{sums.last}"
-puts "part2: #{sums.reverse.take(3).inject{|sum,i| sum+=i}}"
+require 'httparty'                                                                                         
+require 'pry'                                                                                              
+                                                                                                           
+def calculatePoints(chooses)
+  # my choose is the second char
+  gamePoints = case chooses.chars.sort.join
+    when "AZ", "BX", "CY"
+      0
+    when "AX", "BY", "CZ"
+      3
+    when "AY", "BZ", "CX"
+      6
+  end
+  choosePoints = case chooses.chars.sort.last
+    when "X"
+      1
+    when "Y"
+      2
+    when "Z"
+      3
+  end
+  points = gamePoints+choosePoints
+  puts "for chooses #{chooses.chars.sort.join} #{gamePoints}+#{choosePoints}=#{points}"
+  points                                                                                               
+end                                                                                                        
+                                                                                                           
+lines = File.open("input2.txt").read.split("\n")                                 
+results = lines.map{|line| calculatePoints(line.gsub(" ", ""))}                                                
+puts "part1: #{results.inject{|sum, r| sum+=r}}"                                                                                 
+#puts "part2: #{sums.reverse.take(3).inject{|sum,i| sum+=i}}"                                               
+                                                              
