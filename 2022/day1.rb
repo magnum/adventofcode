8a79f746627f4f8e6bcc691839107bdc9da1440e
Antonio Molinari <molinari@incode.it>
2022-12-05 01:22:58 +0100

    improvements to prepare script

--- 2022/day1.rb
+++ 2022/day1.rb
@@ -1,7 +1,7 @@
 require 'httparty'
 require 'pry'
 
-#lines = HTTParty.get 'https://adventofcode.com/2022/day/1/input
+#lines = HTTParty.get 'https://adventofcode.com/2022/day/1/input'
 lines = File.open("day.txt").read.split("\n\n").map{|l| l.split("\n")}
 sums = lines.map{|a| a.inject(0){|sum,b| sum+=b.to_i}}.sort
 puts "part1: #{sums.last}"
--- 2022/prepare.sh
+++ 2022/prepare.sh
@@ -6,7 +6,8 @@ if [ -z "$1" ]
     exit
 fi
 
-echo "
+code_filename="day$1.rb"
+template="
 require 'pry'                        
                                              
 lines = File.open('day$1.txt').read
@@ -16,8 +17,19 @@ result1 = nil
 result2 = nil
 puts \"part1: #{result1}\" if result1   
 puts \"part2: #{result2}\" if result2 
-" > "day$1.rb"
+"
+if [ ! -f $code_filename ]
+then
+  > "day$1.rb"
+else
+  echo "file $code_filename already exists, keeping it..."
+fi  
 
-touch "day$1.md"
+#touch "day$1.md"
+#https://github.com/GreenLightning/advent-of-code-downloader
+aocdl -day $1 -output day$1.txt -force
+
 touch "day$1.txt"
 touch "day$1.test.txt"
+
+
