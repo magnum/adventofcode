e780ee8c15792d558c140425dd56b27dd41567b0
Antonio Molinari <molinari@incode.it>
2022-12-08 00:21:28 +0100

    enter in main

---
+++ 2022/day8.rb
@@ -0,0 +1,11 @@
+
+require "pry"                        
+                                             
+lines = File.open("day8.txt").read
+.split("\n")   
+#binding.pry
+result1 = nil
+result2 = nil
+puts "part1: #{result1}" if result1   
+puts "part2: #{result2}" if result2 
+
