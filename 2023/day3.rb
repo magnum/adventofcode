170aa14bea62eedda3e7f87e5a0e5679c599cabb
Antonio Molinari <molinari@incode.it>
2023-12-11 03:52:47 +0100

    day3 part1

--- 2023/day3.rb
+++ 2023/day3.rb
@@ -1,12 +1,13 @@
 
 require "pry"              
 
-def getSymbolsAround(values, number, xlast, ylast)
+def getSymbolsAround(values, number, cindex, rindex)
   arounds = []
-  ([0,ylast-1].max..[values.length,ylast+1].min).to_a.each do |y|
-    arounds += values[y][[0,(xlast-number.length-1)].max..[xlast, values[y].length].min] if values[y] 
+  ([0,rindex-1].max..[values.length,rindex+1].min).to_a.each do |y|
+    arounds += values[y][[0,(cindex-number.length)].max..[cindex+1, values[y].length].min] if values[y] 
   end
-  #binding.pry
+  binding.pry if number == "469"
+ 
   arounds.join("").gsub(number,"").split("").join("")
 end
 
@@ -16,15 +17,15 @@ values = File.open("day3.txt").read
 .map{ _1.split ""}
 
 result1 = 0
+lastx, lasty = 0
 values.each_with_index do |row, rindex|
   row.each_with_index do |cel, cindex|
     if cel.match?(/\d/)
       numbers[numbers.length-1] += cel  
-      next
-    end
-    if numbers[numbers.length-1] != ""
+      lastx, lasty = cindex, rindex
+    elsif numbers[numbers.length-1] != ""
       number = numbers[numbers.length-1]
-      symbolsAround = getSymbolsAround(values, number, cindex, rindex)
+      symbolsAround = getSymbolsAround(values, number, lastx, lasty)
       puts "found #{number}, symbolsAround: #{symbolsAround}"
       #binding.pry
       result1 += number.to_i if symbolsAround.split("").uniq.join() != "."
