b4dc116058f6fbf869cb4676b9a50ea5b1b74c62
Antonio Molinari <molinari@incode.it>
2023-12-11 03:26:50 +0100

    wip day3

--- 2023/day3.rb
+++ 2023/day3.rb
@@ -1,20 +1,38 @@
 
-require "pry"                        
-                                             
-lines = File.open("day3.txt").read
-.split("\n")   
+require "pry"              
 
-lines.each_with_index do |line,indexl|
-  numbers = []
-  line.each_char.with_index do |c, indexc|
-    numbers << "" if (indexc == 0 || line.chars[indexc-1] == "." rescue false)
-    numbers[numbers.size-1] += c if c =~ /[0-9]/
+def getSymbolsAround(values, number, xlast, ylast)
+  arounds = []
+  ([0,ylast-1].max..[values.length,ylast+1].min).to_a.each do |y|
+    arounds += values[y][[0,(xlast-number.length-1)].max..[xlast, values[y].length].min] if values[y] 
   end
-  numbers.filter!{ _1 != ""}
-  puts "line#{indexl}, numbers: #{numbers}"
+  #binding.pry
+  arounds.join("").gsub(number,"").split("").join("")
 end
 
-result1 = nil
+numbers = [""]                             
+values = File.open("day3.txt").read
+.split("\n")   
+.map{ _1.split ""}
+
+result1 = 0
+values.each_with_index do |row, rindex|
+  row.each_with_index do |cel, cindex|
+    if cel.match?(/\d/)
+      numbers[numbers.length-1] += cel  
+      next
+    end
+    if numbers[numbers.length-1] != ""
+      number = numbers[numbers.length-1]
+      symbolsAround = getSymbolsAround(values, number, cindex, rindex)
+      puts "found #{number}, symbolsAround: #{symbolsAround}"
+      #binding.pry
+      result1 += number.to_i if symbolsAround.split("").uniq.join() != "."
+      numbers << "" 
+    end
+  end
+end
+
 result2 = nil
 puts "part1: #{result1}" if result1   
 puts "part2: #{result2}" if result2 
