4c97bf22ea1ae1c765add2a73e00cfb40085320b
Antonio Molinari <molinari@incode.it>
2024-12-02 13:56:27 +0100

    2024, day1

---
+++ 2024/day1.rb
@@ -0,0 +1,28 @@
+
+require "pry"                      
+list1, list2 = [], []                                 
+lines = File.open("day1.txt").read
+.split("\n")   
+.each do |line| 
+  a,b = line.split(" ").map(&:strip).map(&:to_i)
+  list1 << a
+  list2 << b
+end
+#part1
+difs = []    
+list1.sort.each_with_index do |a, index|
+  b = list2.sort[index]
+  difs << (b - a).abs
+end
+#part2
+counts = []
+list1.each do |a|
+  counts << a*list2.count(a)
+end
+puts counts
+#binding.pry
+result1 = difs.sum
+result2 = counts.sum
+puts "part1: #{result1}" if result1   
+puts "part2: #{result2}" if result2 
+
