6a945b8b28b4ef9a8c39001f16f8a230eb35d40c
Antonio Molinari <antoniomolinari@me.com>
2025-12-02 23:13:13 +0100

    day2, part2

--- 2025/day2.rb
+++ 2025/day2.rb
@@ -14,21 +14,41 @@ end
 input = File.open(filename).read.strip
 lines = input.split(",")
 
-result = 0
+result1 = 0
+result2 = 0
 lines.each do |range|
   min, max = range.split("-").map(&:to_i)
   
   (min..max).each do |n|
     str = n.to_s
+    length = str.length
   
-    if str.length.even?
-      half = str.length / 2
+    # Part 1
+    if length.even?
+      half = length / 2
       if str[0...half] == str[half..-1]
-        result += n
-        puts "#{n} invalid" if @debug
+        result1 += n
+        puts "#{n} invalid (part1)" if @debug
       end
     end
+    
+    # Part 2
+    invalid = false
+    (2..length).each do |k|
+      next unless length % k == 0
+      part_length = length / k
+      first_part = str[0...part_length]
+      same = true
+      (1...k).each do |i|
+        part = str[i * part_length...(i + 1) * part_length]
+        same = false if part != first_part
+      end
+      invalid = true if same
+    end
+    
+    result2 += n if invalid
   end
 end
 
-puts "result: #{result}"
+puts "part1: #{result1}" if result1
+puts "part2: #{result2}" if result2
